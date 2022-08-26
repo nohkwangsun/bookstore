b8f2bcc7420b4dd48d8cbf15e61eaf05c5c52a99
Kwangsun Noh <nohkwangsun@gmail.com>
2022-08-26 13:01:30 +0800

    update git ignore and delete/add a book

--- bookstore/src/com/onlinejava/project/bookstore/BookStore.java
+++ bookstore/src/com/onlinejava/project/bookstore/BookStore.java
@@ -1,11 +1,21 @@
 package com.onlinejava.project.bookstore;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class BookStore {
 
+
+    private List<Book> bookList;
+    {
+        bookList = new ArrayList<>();
+        bookList.add(new Book("홍길동", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번"));
+        bookList.add(new Book("홍길동1", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번"));
+        bookList.add(new Book("홍길동2", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번"));
+    }
 
     public void printWelcomePage() {
         System.out.println();
@@ -24,7 +34,11 @@ public class BookStore {
         System.out.println("=            |                                  |                    =");
         System.out.println("=            |     2. Book Search               |                    =");
         System.out.println("=            |                                  |                    =");
-        System.out.println("=            |     0. Quit                      |                    =");
+        System.out.println("=            |     3. Add new book              |                    =");
+        System.out.println("=            |                                  |                    =");
+        System.out.println("=            |     4. Delete a book             |                    =");
+        System.out.println("=            |                                  |                    =");
+        System.out.println("=            |     q. Quit                      |                    =");
         System.out.println("=            |                                  |                    =");
         System.out.println("=            ------------------------------------                    =");
         System.out.println("=                                                                    =");
@@ -43,6 +57,33 @@ public class BookStore {
                 String keyword = scanner.nextLine();
                 searchBook(keyword);
                 break;
+            case "3":
+                System.out.printf("Type title:");
+                String title = scanner.nextLine().trim();
+
+                System.out.printf("Type writer:");
+                String writer = scanner.nextLine().trim();
+
+                System.out.printf("Type publisher:");
+                String publisher = scanner.nextLine().trim();
+
+                System.out.printf("Type price:");
+                int price = Integer.parseInt( scanner.nextLine().trim() );
+
+                System.out.printf("Type releaseDate:");
+                String releaseDate = scanner.nextLine().trim();
+
+                System.out.printf("Type location:");
+                String location = scanner.nextLine().trim();
+
+                Book newBook = new Book(title, writer, publisher, price, releaseDate, location);
+                createBook(newBook);
+                break;
+            case "4":
+                System.out.printf("Type title:");
+                String deletingTitle = scanner.nextLine().trim();
+                deleteBook(deletingTitle);
+                break;
             case "q":
                 System.exit(0);
                 break;
@@ -51,6 +92,25 @@ public class BookStore {
         }
         System.out.println("Press enter for the menu...");
         scanner.nextLine();
+    }
+
+    private void deleteBook(String deletingTitle) {
+//        List<Book> list = getBookList();
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i).getTitle().equals(deletingTitle)) {
+//                list.remove(i);
+//                return;
+//            }
+//        }
+
+        getBookList().stream()
+                .filter(book -> book.getTitle().equals(deletingTitle))
+                .findFirst()
+                .ifPresent(getBookList()::remove);
+    }
+
+    private void createBook(Book newBook) {
+        getBookList().add(newBook);
     }
 
     private void searchBook(String keyword) {
@@ -85,11 +145,7 @@ public class BookStore {
     }
 
     private List<Book> getBookList() {
-        return List.of(
-                new Book("홍길동", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번"),
-                new Book("홍길동1", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번"),
-                new Book("홍길동2", "작자미상", "조선사", 10000, "01/01/1500", "2층 10번")
-        );
+        return bookList;
     }
 
 }
